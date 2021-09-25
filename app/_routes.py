1d782b12f7d71f56f4ec05a0f2772ae945ad40cb
nandranx <43058966+nandranx@users.noreply.github.com>
2021-09-24 19:44:25 -0700

    beginning of routine entries

--- app/_routes.py
+++ app/_routes.py
@@ -9,7 +9,7 @@ import random
 import uuid
 
 from app.login import LoginForm
-from app.routine import RoutineForm, RoutineScreenField
+from app.routine import RoutineForm, RoutineScreenField, my_view, add_routine_screen
 from app.connect import ConnectForm, create_link_token
 from app.models import User, RoutineScreen, RoutineEntry
 from app.register import RegistrationForm
@@ -19,7 +19,7 @@ from flask_login import current_user, login_user, login_required, logout_user
 
 from flask_wtf import FlaskForm
 
-from flask import request
+from flask import request,  make_response
 from werkzeug.urls import url_parse
 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
@@ -74,10 +74,10 @@ def connect() :
         return redirect(url_for('connect'))
     return render_template('connect.html', title='Home',form=form, link_token=create_link_token())
 
+@app.route('/routine/<int:unique_id>', methods = ['GET','POST'])
+@app.route('/routine/<int:unique_id>/', methods = ['GET','POST'])
 @app.route('/routine', defaults={'unique_id': 0},    methods = ['GET','POST'])
 @app.route('/routine/', defaults={'unique_id': 0}, methods = ['GET','POST'])
-@app.route('/routine/<unique_id>', methods = ['GET','POST'])
-@app.route('/routine/<unique_id>/', methods = ['GET','POST'])
 @login_required
 def routine(unique_id):
     info = []
@@ -88,14 +88,35 @@ def routine(unique_id):
 
     if unique_id!=0:
         if form.validate_on_submit():
-            for each_field in form:
-                if each_field.type == "StringField" or each_field.type == "PasswordField":
-                    FieldEntry = {str(each_field.name) : str(each_field.data)}
-                    FieldEntries.append(FieldEntry)
-            flash(json.dumps(FieldEntries))
+            #Add a new Routine to the database
+            #for each_field in form:
+            #    if each_field.type == "StringField" or each_field.type == "PasswordField":
+            #        FieldEntry = {str(each_field.name) : str(each_field.data)}
+            #        FieldEntries.append(FieldEntry)
+            
+            rs = RoutineScreen()
+            rs.version_ = '1'
+            rs.is_primary = True
 
-            return redirect(url_for('routine', unique_id ))
-        return render_template('routine.html', title='Routine',form=form, unique_id=unique_id, info=info)
+            rs.reader = '1'
+            rs.editor = '1'
+            rs.entry_reader = '1'
+            rs.entry_editor = '1'
+            
+            rs.name = form.name.data
+            rs.label = form.label.data
+            rs.description = form.description.data
+            rs.fields = form.fields.data
+
+            try:
+                db.session.add(rs)
+                db.session.commit()
+            except Exception as error:
+                info.append(error)
+
+            return redirect(url_for('routine', unique_id=unique_id )), info
+
+        return render_template('routine_entry_editor.html', title='Routine',form=form, unique_id=unique_id, info=info)
     else:
         routines = []
         rs_filtered = RoutineScreen.query.all() 
@@ -106,123 +127,9 @@ def routine(unique_id):
 
         return render_template('routine_index.html', title='Available Routines', routines=routines)
         
-def my_view(info, unique_id):
-    class F(RoutineForm):
-        pass
 
-    # F.username = StringField('username')
-    # setattr(F, 'name_mv2', StringField('title_mv2'))
-    
-    try:
-        rs = RoutineScreen.query.filter_by(id=unique_id).first()
-        fields=[]
-        fields = json.loads(rs.fields)
-        # flash (len(fields))
-        # info.append('**********')
-        for j in range(0, len(fields)):
-            if fields[j]['validators'] == 'DataRequired':
-                field_validators = DataRequired()
 
-            if fields[j]['validators'] == '':
-                field_validators = Optional()
-
-            if fields[j]['type']:
-                setattr(F, fields[j]['name'],StringField(fields[j]['label'], validators=[field_validators]))
-                    
-        setattr(F, 'create_request', SubmitField('Create Request'))
-
-        return F(), info
-
-    except Exception as error:
-        info.append("error : " + "No such form exists. Please select the right form from url")
-        return F(), info
-
-def add_routine_screen(unique_id, info):
-    array = [
-        { 
-        "name" : 'id',
-        "label" : 'id',
-        "type" : 'Integer',
-        "validators" : ''
-        },
-        { 
-        "name" : 'version',
-        "label" : 'version',
-        "type" : 'Integer',
-        "validators" : ''
-        },
-        { 
-        "name" : 'is_primary',
-        "label" : 'is_primary',
-        "type" : 'Boolean',
-        "validators" : ''
-        },
-        { 
-        "name" : 'name',
-        "label" : 'name',
-        "type" : 'String',
-        "validators" : ''
-        },
-        { 
-        "name" : 'label',
-        "label" : 'label',
-        "type" : 'String',
-        "validators" : ''
-        },
-        { 
-        "name" : 'description',
-        "label" : 'description',
-        "type" : 'String',
-        "validators" : ''
-        },
-        { 
-        "name" : 'fields',
-        "label" : 'fields',
-        "type" : 'String',
-        "validators" : ''
-        },
-        { 
-        "name" : 'reader',
-        "label" : 'reader',
-        "type" : 'Integer',
-        "validators" : ''
-        },
-        { 
-        "name" : 'editor',
-        "label" : 'editor',
-        "type" : 'Integer',
-        "validators" : ''
-        },
-        { 
-        "name" : 'entry_reader',
-        "label" : 'entry_reader',
-        "type" : 'Integer',
-        "validators" : ''
-        },
-        { 
-        "name" : 'entry_editor',
-        "label" : 'entry_editor',
-        "type" : 'Integer',
-        "validators" : ''
-        }]
-
-    rs = RoutineScreen()
-    rs.id_num = unique_id
-    rs.version_ = '1'
-    rs.is_primary = True
-    rs.reader = '1'
-    rs.editor = '1'
-    rs.entry_reader = '1'
-    rs.entry_editor = '1'
-    rs.name = 'RoutineScreen_form_name_{}'.format(unique_id)
-    rs.label = 'label_{}'.format(unique_id)
-    rs.description = 'description{}'.format(unique_id)
-    rs.fields = json.dumps(array,indent=4)
-
-    try:
-        db.session.add(rs)
-        db.session.commit()
-    except Exception as error:
-        info.append(error)
-    return info
-
+@app.errorhandler(404)
+def not_found(e):
+    """Page not found."""
+    return make_response(render_template("404.html"),404)
--- app/routine.py
+++ app/routine.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 import base64
 import os
 import datetime
 import plaid
 import json
 import time
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import jsonify
+from app import app
+from app import db
+from app.models import User, RoutineScreen, RoutineEntry
+from flask import Flask, make_response, render_template, request, jsonify
 
 class RoutineForm(FlaskForm):
 
@@ -31,5 +31,121 @@ class RoutineScreenField():
 
     def __repr__(self):
         return '<RoutineScreenField {}>'.format(self.name)
+
+
+def my_view(info, unique_id):
+    class F(RoutineForm):
+        pass
+
+    # F.username = StringField('username')
+    # setattr(F, 'name_mv2', StringField('title_mv2'))
+    
+    try:
+        rs = RoutineScreen.query.filter_by(id=unique_id).first()
+        fields=[]
+        fields = json.loads(rs.fields)
+        # flash (len(fields))
+        # info.append('**********')
+        for j in range(0, len(fields)):
+            if fields[j]['validators'] == 'DataRequired':
+                field_validators = DataRequired()
+
+            if fields[j]['validators'] == '':
+                field_validators = Optional()
+
+            if fields[j]['type']:
+                setattr(F, fields[j]['name'],StringField(fields[j]['label'], validators=[field_validators]))
+                    
+        setattr(F, 'create_request', SubmitField('Create Request'))
+
+        return F(), info
+
+    except Exception as error:
+        info.append("error : " + "No such form exists. Please select the right form from url")
+        return F(), info
+
+def add_routine_screen(unique_id, info):
+    array = [
+        { 
+        "name" : "version",
+        "label" : "version",
+        "type" : "Integer",
+        "validators" : ""
+        },
+        { 
+        "name" : "is_primary",
+        "label" : "is_primary",
+        "type" : "Boolean",
+        "validators" : ""
+        },
+        { 
+        "name" : "name",
+        "label" : "name",
+        "type" : "String",
+        "validators" : ""
+        },
+        { 
+        "name" : "label",
+        "label" : "label",
+        "type" : "String",
+        "validators" : ""
+        },
+        { 
+        "name" : "description",
+        "label" : "description",
+        "type" : "String",
+        "validators" : ""
+        },
+        { 
+        "name" : "fields",
+        "label" : "fields",
+        "type" : "String",
+        "validators" : ""
+        },
+        { 
+        "name" : "reader",
+        "label" : "reader",
+        "type" : "Integer",
+        "validators" : ""
+        },
+        { 
+        "name" : "editor",
+        "label" : "editor",
+        "type" : "Integer",
+        "validators" : ""
+        },
+        { 
+        "name" : "entry_reader",
+        "label" : "entry_reader",
+        "type" : "Integer",
+        "validators" : ""
+        },
+        { 
+        "name" : "entry_editor",
+        "label" : "entry_editor",
+        "type" : "Integer",
+        "validators" : ""
+        }]
+
+    rs = RoutineScreen()
+    rs.version_ = '1'
+    rs.is_primary = True
+
+    rs.reader = '1'
+    rs.editor = '1'
+    rs.entry_reader = '1'
+    rs.entry_editor = '1'
+    
+    rs.name = 'RoutineScreen_form_name_{}'.format(unique_id)
+    rs.label = 'label_{}'.format(unique_id)
+    rs.description = 'description_{}'.format(unique_id)
+    rs.fields = json.dumps(array,indent=4)
+
+    try:
+        db.session.add(rs)
+        db.session.commit()
+    except Exception as error:
+        info.append(error)
+    return info
 
 
